f3894529465a567a922e217702ff78a960e3e86f
Anton Astafiev <asa@matterlabs.dev>
2023-12-05 11:45:24 +0100

    Remove debug prints

--- day5/5-2.py
+++ day5/5-2.py
@@ -4,7 +4,7 @@ s = [int(x) for x in f.readline().strip().split(': ')[1].split()]
 seeds = []
 for i in range(len(s) // 2):
     seeds.append((s[i*2], s[i*2] + s[i*2+1]))
-#print(seeds)
+
 f.readline()
 f.readline()
 
@@ -24,24 +24,20 @@ def apply_mapping(mapping, seeds):
     output = []
     for s in seeds:
         for fr, to, r, sh in mapping:
-            #print(fr,to,r,sh)
-            #print(s)
             if s[0] >= fr and s[0] < fr + r:
                 if s[1] > fr + r - 1:
                     output.append((s[0] + sh, to + r - 1))
                     s = (to + r - sh, s[1])
-                    #print(s)
                 else:
                     output.append((s[0] + sh, s[1] + sh))
                     s = ()
                     break
         if s:
             output.append(s)
-        #print(output)
+
     return output
 
 for i in range(7):
-    #print('-------')
     mapping = read_mapping(f)
     seeds = apply_mapping(mapping, seeds)
 
