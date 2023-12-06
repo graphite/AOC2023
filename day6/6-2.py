ede2f1314b52c7320d947c5efbe83cae282a86c0
Anton Astafiev <asa@matterlabs.dev>
2023-12-06 07:57:36 +0100

    Binary search solution

--- day6/6-2.py
+++ day6/6-2.py
@@ -3,8 +3,18 @@ f = open('input6.txt')
 time = int(''.join(f.readline().strip().split()[1:]))
 dist = int(''.join(f.readline().strip().split()[1:]))
 
-for i in range(time):
-    if (time - i) * i > dist:
-        break
+# Slow solution
+# for i in range(time):
+#    if (time - i) * i > dist:
+#        break
+
+i, j = 0, time
+
+while j - i > 1:
+    n = (i + j) // 2
+    if (time - n) * n > dist:
+        j = n
+    else:
+        i = n
 
 print(time - 2 * (i - 1) - 1)
