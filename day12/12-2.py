7c25ea68222b88f16ee93177fe16ae0def7f9978
Anton Astafiev <asa@matterlabs.dev>
2023-12-12 11:55:32 +0100

    Soution day 12

---
+++ day12/12-2.py
@@ -0,0 +1,58 @@
+f = open('input12.txt')
+
+n = 0
+
+CACHE = {}
+
+def permutations(row, nums):
+    row = row.strip('.')
+    if not nums:
+        if '#' in row:
+            return 0
+        else:
+            return 1
+    if len(nums) == 1:
+        if nums[0] == len(row):
+            if '.' in row:
+                CACHE[(row, nums)] = 0
+                return 0
+            else:
+                CACHE[(row, nums)] = 1
+                return 1
+    if (row, nums) in CACHE:
+        return CACHE[(row, nums)]
+    n = nums[0]
+    br = row.find('#')
+    if br == -1:
+        br = len(row)
+    for i in range(len(row) - n + 1):
+        if i > br:
+            CACHE[(row, nums)] = 0
+            return 0
+        if '.' not in row[i:i+n]:
+            if i + n < len(row) and row[i + n] != '#':
+                break
+            if i + n == len(row):
+                break
+    else:
+        CACHE[(row, nums)] = 0
+        return 0
+    if i + n == len(row):
+        if len(nums) > 1:
+            CACHE[(row, nums)] = 0
+            return 0
+        else:
+            CACHE[(row, nums)] = 1
+            return 1
+    CACHE[(row, nums)] = permutations(row[i + n + 1:], nums[1:]) + (permutations(row[i + 1:], nums) if row[i] != '#' else 0)
+    return CACHE[(row, nums)]
+
+for l in f:
+    l = l.strip()
+    row, nums = l.split()
+    row = '?'.join([row,row,row,row,row])
+    nums = ','.join([nums,nums,nums,nums,nums])
+    nums = tuple(int(x) for x in nums.split(','))
+    n += permutations(row, nums)
+
+print(n)
